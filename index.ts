6367ddbe12d38cc6e98ab0170d7f4f06454c7c52
Muhammad Ali <momij299@gmail.com>
2024-04-24 02:32:21 +0500

    Project Completed

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// computer generated value
+let computerNumber = Math.floor(Math.random() * 10);
+
+// get input value in answer
+let answer = await inquirer.prompt([
+    {
+        name : 'number',
+        type : 'input' , 
+        message : 'Enter Number to Guess In Between 1 to 10 : ',
+    }
+
+])
+
+// check if match
+if(computerNumber == answer.number){
+    console.log(`Congratulations! You Win : Computer You Selected ${answer.number} & Computer Selected ${computerNumber}`);
+}
+else{
+    console.log(`Sorry! You Lose : Computer You Selected ${answer.number} & Computer Selected ${computerNumber}`);
+}
+
